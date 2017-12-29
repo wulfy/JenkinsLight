967188c9b1261a353cd4538a1fb9f7a2253407ef
root <root@ns354179.ip-91-121-101.eu>
2017-12-29 17:00:13 +0100

    last update

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,7 +55,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 3000,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: '0.0.0.0',
                 livereload: 35729
